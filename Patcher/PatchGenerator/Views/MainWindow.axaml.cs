00463d4a942a370b3d6930aa5904ebaf5080d738
waffle.lord <waffle.lord@hotmail.com>
2024-03-07 15:56:40 -0500

    update patch gen/client to net 8

--- Patcher/PatchGenerator/Views/MainWindow.axaml.cs
+++ Patcher/PatchGenerator/Views/MainWindow.axaml.cs
@@ -11,9 +11,6 @@ namespace PatchGenerator.Views
         public MainWindow()
         {
             InitializeComponent();
-#if DEBUG
-            this.AttachDevTools();
-#endif
         }
 
         private void InitializeComponent()
